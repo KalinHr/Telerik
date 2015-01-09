4b5f876c4fea0813f45511577c0c0e2f30f9eaf5
KalinHr <kalinn_1991_@gmail.com>
2015-01-09 14:26:37 +0200

    Hello, C#

---
+++ 1. C# Programming - Part I/1. Introduction to Programming/1.HelloCSharp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/*Create, compile and run a “Hello C#” console application.*/
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello C#");
+    }
+}
+
