e3693cece70c0d41247b674b1d10041b0fd6bd1b
KalinHr <kalinn_1991_@gmail.com>
2015-01-13 20:35:31 +0200

    odd or even?

---
+++ 1. C# Programming - Part I/3. Operators and Expressions/01.OddOrEvenIntegers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*Write an expression that checks if given integer is odd or even.*/
+
+class OddOrEvenIntegers
+{
+    static void Main()
+    {
+        Console.Write("number = ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        bool even;
+        if (number % 2 == 0)
+            even = true;
+        else
+            even = false;
+        Console.WriteLine(even);
+    }
+}
+
