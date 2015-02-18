b1cd456a8b59b7ae98b60b9b88cfd59107fe3034
KalinHr <kalinn1991@gmail.com>
2015-02-18 11:18:49 +0200

    ...

---
+++ 2. C# Programming - Part 2/2.MultidimensionalArrays/01.FillTheMatrix.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+/*
+ Write a program that fills and prints a matrix of size (n, n) as shown below:
+ Example for n=4:
+
+a)
+1	5	9	13
+2	6	10	14
+3	7	11	15
+4	8	12	16
+b)
+1	8	9	16
+2	7	10	15
+3	6	11	14
+4	5	12	13
+c)
+7	11	14	16
+4	8	12	15
+2	5	9	13
+1	3	6	10
+ */
+
+class Program
+{
+    static void PrintMatrix(int[,] matrix, int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+                Console.Write(matrix[i, j] + " ");
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    static void Main()
+    {
+        Console.Write("size = ");
+        int size = int.Parse(Console.ReadLine());
+
+        //a
+        int counter = 1;
+        int[,] a = new int[size, size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+            {
+                a[j, i] = counter;
+                counter++;
+            }
+
+        //b
+        counter = 1;
+        int[,] b = new int[size, size];
+        for (int i = 0; i < size; i++)
+            if (i % 2 == 0)
+                for (int j = 0; j < size; j++)
+                {
+                    b[j, i] = counter;
+                    counter++;
+                }
+            else
+                for (int j = size - 1; j >= 0; j--)
+                {
+                    b[j, i] = counter;
+                    counter++;
+                }
+
+        //c
+        counter = 1;
+        int[,] c = new int[size, size];
+        for (int i = size - 1; i >= 0; i--)
+            for (int j = i; j < size; j++)
+            {
+                c[j, j - i] = counter;
+                counter++;
+            }
+        for (int i = -1; i >= 1 - size; i--)
+            for (int j = 0; j <= size + i - 1; j++)
+            {
+                c[j, j - i] = counter;
+                counter++;
+            }
+
+        PrintMatrix(a, size);
+        PrintMatrix(b, size);
+        PrintMatrix(c, size);
+    }
+}
---
+++ 2. C# Programming - Part 2/2.MultidimensionalArrays/02.MaximalSum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/*Write a program that reads a rectangular matrix of size N x M and finds in it the square 3 x 3 that 
+  has maximal sum of its elements.*/
+
+class Program
+{
+    static int SumOfMatrix(int[,] matrix, int x, int y)
+    {
+        int sum = 0;
+        for (int i = x; i < x + 3; i++)
+            for (int j = y; j < y + 3; j++)
+                sum += matrix[i, j]; 
+        return sum;
+    }
+
+    static void PrintMatrix(int[,] matrix, int x, int y, int n, int m)
+    {
+        for (int i = x; i < x + n; i++)
+        {
+            for (int j = y; j < y + m; j++)
+                Console.Write(matrix[i, j] + " ");
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("M = ");
+        int m = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[n,m]; 
+
+        Random random = new Random();
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                matrix[i, j] = random.Next(0, 100);
+
+        PrintMatrix(matrix, 0, 0, n, m);
+
+        int x = 0;
+        int y = 0;
+        int sum = 0;
+        for (int i = 0; i < n - 2; i++)
+            for (int j = 0; j < m - 2; j++)
+                if (SumOfMatrix(matrix, i, j) > sum)
+                {
+                    x = i;
+                    y = j;
+                    sum = SumOfMatrix(matrix, i, j);
+                }
+
+        PrintMatrix(matrix, x, y, 3, 3);
+    }
+}
